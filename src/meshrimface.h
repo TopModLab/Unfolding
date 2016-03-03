5db9eb9636331fb530a5499ab42280408627c47e
Wu <urianawu@tamu.edu>
2016-03-03 14:25:56 -0600

    in the process of refactoring RimFace, do not compile

--- src/meshrimface.h
+++ src/meshrimface.h
@@ -13,6 +13,18 @@ public:
 	static void rimMesh3D(HDS_Mesh *mesh, float planeWidth, float planeHeight);
 	~MeshRimFace();
 
+	void computePlaneCornerOnEdge(vert_t* v, he_t* he, vector<QVector3D> &vpos);
+	void computePlaneCornerOnFace(vert_t* v, he_t* he, vector<QVector3D> &control_points_p, vector<QVector3D> &control_points_n);
+
+	enum RimType {
+		EdgeWithBezier = 0,
+		EdgeWithCubic = 1,
+		FaceWithCubic = 2
+	};
+
+private:
+	float planeWidth;
+	float planeHeight;
 };
 
 #endif // MESHRIMFACE_H
