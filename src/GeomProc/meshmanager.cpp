5276355007aad82f44e962a72604210087b6f4f8
ShenyaoKe <akika@tamu.edu>
2016-09-18 18:30:07 -0500

    fix null face filling issue in mesh loading

--- src/GeomProc/meshmanager.cpp
+++ src/GeomProc/meshmanager.cpp
@@ -321,6 +321,20 @@ HDS_Mesh * MeshManager::buildHalfEdgeMesh(
 				null_hes.push_back(curHE);
 				null_hefs.push_back(new he_t);
 				
+				// if curHE->next->flip == null,
+				//     found the next exposed edge
+				///                    ___curHE___
+				///                   /
+				///  exposed edge--> / curHE->next
+				///                 /
+				// else, move to curHE->next->flip->next
+				///                    \    <--exposed edge
+				///         curHE->nex  \
+				///         ->flip->next \  ___curHE___
+				///                      / /
+				///         curHE->next / /curHE->next
+				///         ->flip     / /
+				curHE = curHE->next;
 				// Loop adjacent edges to find the exposed edge
 				while (curHE->flip != nullptr)
 				{
